4818de8e9ed6e497a102e37254c16a7eed5a1e3d
SOOS-JAlvarez <jalvarez@soos.io>
2023-11-30 16:24:19 -0300

    unused consts

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,7 +1,4 @@
 export const CONSTANTS = {
-  FILES: {
-    SARIF_OUTPUT: "results.sarif",
-  },
   SOOS: {
     ENVIRONMENT_VARIABLES: {
       API_KEY: "SOOS_API_KEY",
@@ -9,9 +6,5 @@ export const CONSTANTS = {
     },
     DEFAULT_INTEGRATION_TYPE: "Script",
     DEFAULT_DIRECTORIES_TO_EXCLUDE: ["**/node_modules/**", "**/bin/**", "**/obj/**", "**/lib/**"],
-  },
-  STATUS: {
-    DELAY_TIME: 5000,
-    MAX_ATTEMPTS: 10,
   },
 };
