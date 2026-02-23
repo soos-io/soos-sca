d81b0450dd8b5deccece183c1379e7233686f4c7
SOOS-GSteen <gsteen@soos.io>
2026-02-23 13:44:48 -0500

    immediate async instead of class / package update (#103)

--- src/index.ts
+++ src/index.ts
@@ -38,251 +38,248 @@ interface ISCAAnalysisArgs extends IBaseScanArguments {
   fileMatchType: FileMatchTypeEnum;
 }
 
-class SOOSSCAAnalysis {
-  constructor(private args: ISCAAnalysisArgs) {}
+const parseArgs = (): ISCAAnalysisArgs => {
+  const analysisArgumentParser = AnalysisArgumentParser.create(
+    IntegrationName.SoosSca,
+    IntegrationType.Script,
+    ScanType.SCA,
+    version,
+  );
 
-  static parseArgs(): ISCAAnalysisArgs {
-    const analysisArgumentParser = AnalysisArgumentParser.create(
-      IntegrationName.SoosSca,
-      IntegrationType.Script,
-      ScanType.SCA,
-      version,
-    );
+  analysisArgumentParser.addArgument(
+    "directoriesToExclude",
+    "Listing of directories or patterns to exclude from the search for manifest files. eg: **bin/start/**, **/start/**",
+    {
+      argParser: (value: string) => {
+        return removeDuplicates(value.split(",").map((pattern) => pattern.trim()));
+      },
+      defaultValue: SOOS_SCA_CONSTANTS.DefaultDirectoriesToExclude,
+    },
+  );
 
-    analysisArgumentParser.addArgument(
-      "directoriesToExclude",
-      "Listing of directories or patterns to exclude from the search for manifest files. eg: **bin/start/**, **/start/**",
-      {
-        argParser: (value: string) => {
-          return removeDuplicates(value.split(",").map((pattern) => pattern.trim()));
+  analysisArgumentParser.addArgument(
+    "filesToExclude",
+    "Listing of files or patterns patterns to exclude from the search for manifest files. eg: **/req**.txt/, **/requirements.txt",
+    {
+      argParser: (value: string) => {
+        return value.split(",").map((pattern) => pattern.trim());
+      },
+      defaultValue: [],
+    },
+  );
+
+  analysisArgumentParser.addEnumArgument(
+    "fileMatchType",
+    FileMatchTypeEnum,
+    "The method to use to locate files for scanning, looking for manifest files and/or files to hash.",
+    {
+      defaultValue: FileMatchTypeEnum.Manifest,
+    },
+  );
+
+  analysisArgumentParser.addEnumArgument(
+    "packageManagers",
+    {
+      [PackageManagerType.CFamily]: PackageManagerType.CFamily,
+      [PackageManagerType.Dart]: PackageManagerType.Dart,
+      [PackageManagerType.Erlang]: PackageManagerType.Erlang,
+      [PackageManagerType.Go]: PackageManagerType.Go,
+      [PackageManagerType.Homebrew]: PackageManagerType.Homebrew,
+      [PackageManagerType.Java]: PackageManagerType.Java,
+      [PackageManagerType.NPM]: PackageManagerType.NPM,
+      [PackageManagerType.NuGet]: PackageManagerType.NuGet,
+      [PackageManagerType.Php]: PackageManagerType.Php,
+      [PackageManagerType.Python]: PackageManagerType.Python,
+      [PackageManagerType.Ruby]: PackageManagerType.Ruby,
+      [PackageManagerType.Rust]: PackageManagerType.Rust,
+      [PackageManagerType.Swift]: PackageManagerType.Swift,
+      [PackageManagerType.Unity]: PackageManagerType.Unity,
+    },
+    "A list of package managers, delimited by comma, to include when searching for manifest files.",
+    {
+      allowMultipleValues: true,
+    },
+  );
+
+  analysisArgumentParser.addArgument(
+    "sourceCodePath",
+    "Root path to begin recursive search for manifests.",
+    {
+      defaultValue: process.cwd(),
+    },
+  );
+
+  analysisArgumentParser.addArgument(
+    "workingDirectory",
+    "Absolute path where SOOS may write and read persistent files for the given build. eg Correct: /tmp/workspace/ | Incorrect: ./bin/start/",
+    {
+      defaultValue: process.cwd(),
+    },
+  );
+
+  analysisArgumentParser.addArgument(
+    "outputDirectory",
+    "Absolute path where SOOS will write exported reports and SBOMs. eg Correct: /out/sbom/ | Incorrect: ./out/sbom/",
+    {
+      defaultValue: process.cwd(),
+    },
+  );
+
+  return analysisArgumentParser.parseArguments();
+};
+
+const runAnalysis = async (args: ISCAAnalysisArgs): Promise<void> => {
+  const scanType = ScanType.SCA;
+  const analysisService = AnalysisService.create(args.apiKey, args.apiURL);
+
+  let projectHash: string | undefined;
+  let branchHash: string | undefined;
+  let analysisId: string | undefined;
+  let scanStatusUrl: string | undefined;
+  let scanStatus: ScanStatus | undefined;
+
+  try {
+    const result = await analysisService.setupScan({
+      clientId: args.clientId,
+      projectName: args.projectName,
+      branchName: args.branchName,
+      commitHash: args.commitHash,
+      buildVersion: args.buildVersion,
+      buildUri: args.buildURI,
+      branchUri: args.branchURI,
+      operatingEnvironment: args.operatingEnvironment,
+      integrationName: args.integrationName,
+      integrationType: args.integrationType,
+      appVersion: args.appVersion,
+      scriptVersion: args.scriptVersion,
+      contributingDeveloperAudit: [
+        {
+          contributingDeveloperId: args.contributingDeveloperId,
+          source: args.contributingDeveloperSource,
+          sourceName: args.contributingDeveloperSourceName,
         },
-        defaultValue: SOOS_SCA_CONSTANTS.DefaultDirectoriesToExclude,
-      },
-    );
+      ],
+      scanType,
+      commandLine:
+        process.argv.length > 2
+          ? obfuscateCommandLine(
+              reassembleCommandLine(process.argv.slice(2)),
+              SOOS_SCA_CONSTANTS.ObfuscatedArguments.map((a) => `--${a}`),
+            )
+          : null,
+    });
 
-    analysisArgumentParser.addArgument(
-      "filesToExclude",
-      "Listing of files or patterns patterns to exclude from the search for manifest files. eg: **/req**.txt/, **/requirements.txt",
-      {
-        argParser: (value: string) => {
-          return value.split(",").map((pattern) => pattern.trim());
-        },
-        defaultValue: [],
-      },
-    );
+    projectHash = result.projectHash;
+    branchHash = result.branchHash;
+    analysisId = result.analysisId;
+    scanStatusUrl = result.scanStatusUrl;
 
-    analysisArgumentParser.addEnumArgument(
-      "fileMatchType",
-      FileMatchTypeEnum,
-      "The method to use to locate files for scanning, looking for manifest files and/or files to hash.",
-      {
-        defaultValue: FileMatchTypeEnum.Manifest,
-      },
-    );
+    const { manifestFiles, hashManifests } = await analysisService.findManifestsAndHashableFiles({
+      clientId: args.clientId,
+      projectHash,
+      filesToExclude: args.filesToExclude,
+      directoriesToExclude: args.directoriesToExclude,
+      sourceCodePath: args.sourceCodePath,
+      packageManagers: args.packageManagers ?? [],
+      fileMatchType: args.fileMatchType,
+    });
 
-    analysisArgumentParser.addEnumArgument(
-      "packageManagers",
-      {
-        [PackageManagerType.CFamily]: PackageManagerType.CFamily,
-        [PackageManagerType.Dart]: PackageManagerType.Dart,
-        [PackageManagerType.Erlang]: PackageManagerType.Erlang,
-        [PackageManagerType.Go]: PackageManagerType.Go,
-        [PackageManagerType.Homebrew]: PackageManagerType.Homebrew,
-        [PackageManagerType.Java]: PackageManagerType.Java,
-        [PackageManagerType.NPM]: PackageManagerType.NPM,
-        [PackageManagerType.NuGet]: PackageManagerType.NuGet,
-        [PackageManagerType.Php]: PackageManagerType.Php,
-        [PackageManagerType.Python]: PackageManagerType.Python,
-        [PackageManagerType.Ruby]: PackageManagerType.Ruby,
-        [PackageManagerType.Rust]: PackageManagerType.Rust,
-        [PackageManagerType.Swift]: PackageManagerType.Swift,
-        [PackageManagerType.Unity]: PackageManagerType.Unity,
-      },
-      "A list of package managers, delimited by comma, to include when searching for manifest files.",
-      {
-        allowMultipleValues: true,
-      },
-    );
+    const { exitCode } = await analysisService.addManifestsAndHashableFilesToScan({
+      clientId: args.clientId,
+      projectHash: result.projectHash,
+      branchHash: result.branchHash,
+      analysisId: result.analysisId,
+      scanType,
+      scanStatusUrl: result.scanStatusUrl,
+      fileMatchType: args.fileMatchType,
+      manifestFiles,
+      hashManifests,
+    });
+    if (exitCode !== 0) {
+      exit(exitCode);
+    }
 
-    analysisArgumentParser.addArgument(
-      "sourceCodePath",
-      "Root path to begin recursive search for manifests.",
-      {
-        defaultValue: process.cwd(),
-      },
-    );
+    await analysisService.startScan({
+      clientId: args.clientId,
+      projectHash,
+      analysisId: result.analysisId,
+      scanType,
+      scanUrl: result.scanUrl,
+    });
 
-    analysisArgumentParser.addArgument(
-      "workingDirectory",
-      "Absolute path where SOOS may write and read persistent files for the given build. eg Correct: /tmp/workspace/ | Incorrect: ./bin/start/",
-      {
-        defaultValue: process.cwd(),
-      },
-    );
+    scanStatus = await analysisService.waitForScanToFinish({
+      scanStatusUrl: result.scanStatusUrl,
+      scanUrl: result.scanUrl,
+      scanType,
+    });
 
-    analysisArgumentParser.addArgument(
-      "outputDirectory",
-      "Absolute path where SOOS will write exported reports and SBOMs. eg Correct: /out/sbom/ | Incorrect: ./out/sbom/",
-      {
-        defaultValue: process.cwd(),
-      },
-    );
-
-    return analysisArgumentParser.parseArguments();
-  }
-
-  async runAnalysis(): Promise<void> {
-    const scanType = ScanType.SCA;
-    const analysisService = AnalysisService.create(this.args.apiKey, this.args.apiURL);
-
-    let projectHash: string | undefined;
-    let branchHash: string | undefined;
-    let analysisId: string | undefined;
-    let scanStatusUrl: string | undefined;
-    let scanStatus: ScanStatus | undefined;
-
-    try {
-      const result = await analysisService.setupScan({
-        clientId: this.args.clientId,
-        projectName: this.args.projectName,
-        branchName: this.args.branchName,
-        commitHash: this.args.commitHash,
-        buildVersion: this.args.buildVersion,
-        buildUri: this.args.buildURI,
-        branchUri: this.args.branchURI,
-        operatingEnvironment: this.args.operatingEnvironment,
-        integrationName: this.args.integrationName,
-        integrationType: this.args.integrationType,
-        appVersion: this.args.appVersion,
-        scriptVersion: this.args.scriptVersion,
-        contributingDeveloperAudit: [
-          {
-            contributingDeveloperId: this.args.contributingDeveloperId,
-            source: this.args.contributingDeveloperSource,
-            sourceName: this.args.contributingDeveloperSourceName,
-          },
-        ],
-        scanType,
-        commandLine:
-          process.argv.length > 2
-            ? obfuscateCommandLine(
-                reassembleCommandLine(process.argv.slice(2)),
-                SOOS_SCA_CONSTANTS.ObfuscatedArguments.map((a) => `--${a}`),
-              )
-            : null,
-      });
-
-      projectHash = result.projectHash;
-      branchHash = result.branchHash;
-      analysisId = result.analysisId;
-      scanStatusUrl = result.scanStatusUrl;
-
-      const { manifestFiles, hashManifests } = await analysisService.findManifestsAndHashableFiles({
-        clientId: this.args.clientId,
-        projectHash,
-        filesToExclude: this.args.filesToExclude,
-        directoriesToExclude: this.args.directoriesToExclude,
-        sourceCodePath: this.args.sourceCodePath,
-        packageManagers: this.args.packageManagers ?? [],
-        fileMatchType: this.args.fileMatchType,
-      });
-
-      const { exitCode } = await analysisService.addManifestsAndHashableFilesToScan({
-        clientId: this.args.clientId,
+    if (
+      isScanDone(scanStatus) &&
+      args.exportFormat !== AttributionFormatEnum.Unknown &&
+      args.exportFileType !== AttributionFileTypeEnum.Unknown
+    ) {
+      await analysisService.generateFormattedOutput({
+        clientId: args.clientId,
         projectHash: result.projectHash,
+        projectName: args.projectName,
         branchHash: result.branchHash,
         analysisId: result.analysisId,
-        scanType,
-        scanStatusUrl: result.scanStatusUrl,
-        fileMatchType: this.args.fileMatchType,
-        manifestFiles,
-        hashManifests,
+        format: args.exportFormat,
+        fileType: args.exportFileType,
+        includeDependentProjects: false,
+        includeOriginalSbom: false,
+        includeVulnerabilities: false,
+        workingDirectory: args.outputDirectory,
       });
-      if (exitCode !== 0) {
-        exit(exitCode);
-      }
+    }
 
-      await analysisService.startScan({
-        clientId: this.args.clientId,
+    const { exitCode: analysisExitCode, message } = getAnalysisExitCodeWithMessage(
+      scanStatus,
+      args.integrationName,
+      args.onFailure,
+    );
+    soosLogger.always(`${message} - exit ${analysisExitCode}`);
+    exit(analysisExitCode);
+  } catch (e) {
+    const errorMessage = e instanceof Error ? e.message : (e as string);
+    if (projectHash && branchHash && analysisId && (!scanStatus || !isScanDone(scanStatus)))
+      await analysisService.updateScanStatus({
+        clientId: args.clientId,
         projectHash,
-        analysisId: result.analysisId,
+        branchHash,
         scanType,
-        scanUrl: result.scanUrl,
+        analysisId: analysisId,
+        status: ScanStatus.Error,
+        message: `Error while performing scan: ${errorMessage}`,
+        scanStatusUrl,
       });
-
-      scanStatus = await analysisService.waitForScanToFinish({
-        scanStatusUrl: result.scanStatusUrl,
-        scanUrl: result.scanUrl,
-        scanType,
-      });
-
-      if (
-        isScanDone(scanStatus) &&
-        this.args.exportFormat !== AttributionFormatEnum.Unknown &&
-        this.args.exportFileType !== AttributionFileTypeEnum.Unknown
-      ) {
-        await analysisService.generateFormattedOutput({
-          clientId: this.args.clientId,
-          projectHash: result.projectHash,
-          projectName: this.args.projectName,
-          branchHash: result.branchHash,
-          analysisId: result.analysisId,
-          format: this.args.exportFormat,
-          fileType: this.args.exportFileType,
-          includeDependentProjects: false,
-          includeOriginalSbom: false,
-          includeVulnerabilities: false,
-          workingDirectory: this.args.outputDirectory,
-        });
-      }
-
-      const { exitCode: analysisExitCode, message } = getAnalysisExitCodeWithMessage(
-        scanStatus,
-        this.args.integrationName,
-        this.args.onFailure,
-      );
-      soosLogger.always(`${message} - exit ${analysisExitCode}`);
-      exit(analysisExitCode);
-    } catch (e) {
-      const errorMessage = e instanceof Error ? e.message : (e as string);
-      if (projectHash && branchHash && analysisId && (!scanStatus || !isScanDone(scanStatus)))
-        await analysisService.updateScanStatus({
-          clientId: this.args.clientId,
-          projectHash,
-          branchHash,
-          scanType,
-          analysisId: analysisId,
-          status: ScanStatus.Error,
-          message: `Error while performing scan: ${errorMessage}`,
-          scanStatusUrl,
-        });
-      soosLogger.error(errorMessage);
-      soosLogger.always(`Error: ${errorMessage} - exit 1`);
-      exit(1);
-    }
+    soosLogger.error(errorMessage);
+    soosLogger.always(`Error: ${errorMessage} - exit 1`);
+    exit(1);
   }
+};
 
-  static async createAndRun(): Promise<void> {
-    try {
-      const args = this.parseArgs();
-      soosLogger.setMinLogLevel(args.logLevel);
-      soosLogger.always("Starting SOOS SCA Analysis");
-      soosLogger.debug(
-        JSON.stringify(
-          obfuscateProperties(args as unknown as Record<string, unknown>, ["apiKey"]),
-          null,
-          2,
-        ),
-      );
+const createAndRun = async (): Promise<void> => {
+  try {
+    const args = parseArgs();
+    soosLogger.setMinLogLevel(args.logLevel);
+    soosLogger.always("Starting SOOS SCA Analysis");
+    soosLogger.debug(
+      JSON.stringify(
+        obfuscateProperties(args as unknown as Record<string, unknown>, ["apiKey"]),
+        null,
+        2,
+      ),
+    );
 
-      const soosSCAAnalysis = new SOOSSCAAnalysis(args);
-      await soosSCAAnalysis.runAnalysis();
-    } catch (error) {
-      soosLogger.error(`Error on createAndRun: ${error}`);
-      soosLogger.always(`Error on createAndRun: ${error} - exit 1`);
-      exit(1);
-    }
+    await runAnalysis(args);
+  } catch (error) {
+    soosLogger.error(`Error on createAndRun: ${error}`);
+    soosLogger.always(`Error on createAndRun: ${error} - exit 1`);
+    exit(1);
   }
-}
+};
 
-SOOSSCAAnalysis.createAndRun();
+(async (): Promise<void> => {
+  await createAndRun();
+})();
